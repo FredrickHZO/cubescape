80d3c05dd8fca5a1d1758d55e664f83b7e16c20d
FredrickHZO <federico.armagno97@gmail.com>
2023-08-05 16:33:53 +0200

    New implementation.

--- src/main.c
+++ src/main.c
@@ -7,22 +7,32 @@ const int SCREEN_WIDTH = 400;
 
 int main(void) {
     Camera3D camera = init_camera();
-    Vector3 pos     = {2.0f, 2.0f, 0};
-    Vector3 size    = {2.0f, 2.0f, 2.0f};
+    Vector3 size    = {0.5f, 0.5f, 0.5f};
+    Vector3 pos     = {0, size.y/2, 0};
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cubescape");
     SetTargetFPS(60);
 
+    float points = 1;
+
     while (!WindowShouldClose()) {
         manage_camera_zoom(&camera);
         cube_move(&pos);
+        auto_move_forward(&pos);
+        follow_player_cam(&camera, &pos);
+        points += 0.5;
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
+            DrawText(TextFormat("Score: %.0f", points), 10, 10, 20, BEIGE);
             BeginMode3D(camera);
                 DrawCubeV(pos, size, RED);
                 DrawCubeWiresV(pos, size, BLACK);
-                DrawGrid(20, 10.0f);
+                DrawPlane(
+                    (Vector3){-100, 0, 0},
+                    (Vector2){300, 5},
+                    LIME
+                );
             EndMode3D();
         EndDrawing();
     }
