db4360991820c936a5e427207cd627eb911da1e0
FredrickHZO <federico.armagno97@gmail.com>
2023-08-05 16:33:43 +0200

    Cube forward movement.

--- src/cube.c
+++ src/cube.c
@@ -1,6 +1,6 @@
 #include "cube.h"
 
-const float SPEED = 10.0f;
+const float SPEED = 7.0f;
 
 void move_cube_right(Vector3* pos) {
     pos->z += SPEED * GetFrameTime();
@@ -13,4 +13,12 @@ void move_cube_left(Vector3* pos) {
 void cube_move(Vector3* pos) {
     if (IsKeyDown(KEY_A)) move_cube_left(pos);    
     if (IsKeyDown(KEY_D)) move_cube_right(pos);
+}
+
+void auto_move_forward(Vector3* pos) {
+    pos->x -= SPEED * GetFrameTime();
+}
+
+void random_spawn_cube(Vector3* pos) {
+    
 }
--- src/cube.h
+++ src/cube.h
@@ -2,3 +2,4 @@
 #include <raylib.h>
 
 void cube_move(Vector3* pos);
+void auto_move_forward(Vector3* pos);
