f4a99de7dce2150ee7e8c15daf17b43d6fb1166b
FredrickHZO <federico.armagno97@gmail.com>
2023-08-05 16:33:11 +0200

    Add follow cam.

--- src/camera.c
+++ src/camera.c
@@ -1,16 +1,19 @@
 #include "camera.h"
 
-const float MAX_FOV = 110.0f;
+const float MAX_FOV = 150.0f;
 const float MIN_FOV = 80.0f;
-const float DEF_FOV = 90.0f;
+const float DEF_FOV = 100.0f;
+
+const float CAMERA_X_POS = 2.0f;
+const float CAMERA_Y_POS = 2.5f;
 
 Camera3D init_camera() {
     return (Camera3D) {
-        .position   = (Vector3){ 10.0f, 10.0f, 0 },
-        .target     = (Vector3){ 0.0f, 0.0f, 0.0f },
-        .up         = (Vector3){ 0.0f, 0.1f, 0.0f },
+        .position   = (Vector3){ CAMERA_X_POS, CAMERA_Y_POS, 0 },
+        .target     = (Vector3){0},
+        .up         = (Vector3){ 0.0f, 1.0f, 0.0f },
         .fovy       = DEF_FOV,
-        .projection = CAMERA_PERSPECTIVE
+        .projection = CAMERA_PERSPECTIVE,
     };
 }
 
@@ -26,3 +29,8 @@ void manage_camera_zoom(Camera3D* camera) {
     if (IsKeyDown(KEY_M)) cam_zoom_out(camera);
     if (IsKeyDown(KEY_J)) cam_zoom_in(camera);
 }
+
+void follow_player_cam(Camera3D* camera, Vector3* p_pos) {
+    camera->target.x = p_pos->x - 2.0f;
+    camera->position.x = CAMERA_X_POS + p_pos->x;
+} 
--- src/camera.h
+++ src/camera.h
@@ -3,3 +3,4 @@
 
 Camera3D init_camera();
 void manage_camera_zoom(Camera3D* camera);
+void follow_player_cam(Camera3D* camera, Vector3* p_pos);
