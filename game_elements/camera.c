ed90980b30c82f287132f30b095c2ec456d30031
FredrickHZO <federico.armagno97@gmail.com>
2023-09-16 12:49:35 +0200

    Better camera positioning.

--- game_elements/camera.c
+++ game_elements/camera.c
@@ -1,13 +1,11 @@
 #include "camera.h"
 
-const float MAX_FOV = 150.0f;
-const float MIN_FOV = 80.0f;
-const float DEF_FOV = 100.0f;
+const float DEF_FOV = 40.0f;
 
 const float CAMERA_X_POS = 2.0f;
-const float CAMERA_Y_POS = 2.5f;
-const float PLAYER_DISTANCE = 2.0f;
-const float TARGET_DISTANCE = 2.0f;
+const float CAMERA_Y_POS = 2.0f;
+const float PLAYER_DISTANCE = 5.0f;
+const float TARGET_DISTANCE = 1.0f;
 
 Camera3D init_follow_camera() {
     return (Camera3D) {
@@ -17,19 +15,6 @@ Camera3D init_follow_camera() {
         .fovy       = DEF_FOV,
         .projection = CAMERA_PERSPECTIVE,
     };
-}
-
-void cam_zoom_out(Camera3D* camera) {
-    if (camera->fovy < MAX_FOV) camera->fovy += 1.0f;
-}
-
-void cam_zoom_in(Camera3D* camera) {
-    if (camera->fovy > MIN_FOV) camera->fovy -= 1.0f;
-}
-
-void manage_camera_zoom(Camera3D* camera) {
-    if (IsKeyDown(KEY_M)) cam_zoom_out(camera);
-    if (IsKeyDown(KEY_J)) cam_zoom_in(camera);
 }
 
 void follow_player_cam(Camera3D* camera, struct cube c) {
